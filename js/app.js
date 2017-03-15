d9a387fd50400462ee197163bbbdd8f53b9011c5
Alex Aguinaldo <psionicbones@gmail.com>
2017-03-15 09:03:42 -1000

    update

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// Branches out, starts from first indentation in body, then element(s) inside and then continues
+// Can use {} to go further into indentation if it goes on
+// Make an array if there is more than one element inside that div/header/etc
+// Cannot use html elements inside
+
+
+// var jsonDataBody = [
+// 	{
+// 		"header": "h1"
+
+// 	},
+// 	{
+// 		"div": {
+// 			// attriubte class & value aside
+// 			"class": "aside clearfix"
+// 			// elements that are inside said class go in [] 
+// 		}
+// 	},
+// ];
+
+var simplifiedJson = [
+	{
+		"journalEntry": "1", 
+		"date": "August 15, 2016",
+		"title": "Why I Took This Class"
+		"p": "I took this class to expand my knowledge in coding and use it in the future. Not to mention that poking behind the internet's curtains is pretty great."
+		"img": undefined
+	},
+	{
+		"journalEntry": "2"
+		"date": "August 24, 2016"
+		"title": "Ux vs UI"
+		"p": "UX stands for User Experience and UI stands for User Interface. UX is about the user and their experience using the site itself as opposed to UI which deals with the over all outlook of the site and how it is fashioned."
+		"img": undefined
+	},
+	{
+		"journalEntry": "3"
+		
+	},
+];
